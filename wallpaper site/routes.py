45379542261829bff87bc39bddaf706e5fcf51dc
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-05-19 09:53:32 +1200

    fixed database

--- wallpaper site/routes.py
+++ wallpaper site/routes.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, redirect
 import sqlite3
 
 app = Flask(__name__)
+var = ""
 
 @app.route("/")
 def home(): 
+    
     return render_template("layout.html")
 
 @app.route("/upload.html")
@@ -12,8 +14,11 @@ def upload():
     conn = sqlite3.connect("wallpapersite.db")
     cur = conn.cursor()
     data = cur.execute("SELECT tag FROM photos WHERE tag='d'")                                                                                                                                                               
-    data = cur.fetchall()
-    return render_template("upload.html")
+    data = cur.fetchone()
+    var = data
+    print(data)
+    return render_template("layout.html", var = var[0])
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
