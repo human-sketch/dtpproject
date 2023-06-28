3be3c4c0f24b7cefb8729948c6a765f6bd8ba548
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-06-29 09:25:13 +1200

    done tag upload

--- routes.py
+++ routes.py
@@ -1,31 +1,44 @@
 from flask import Flask, render_template, redirect, url_for, request
+from werkzeug.utils import secure_filename
 import sqlite3
 
 app = Flask(__name__)
-var = ""
-
-
-@app.route("/", methods=['GET', 'POST'])
-def home(): 
-    
-    if request.method == 'POST':
-        data = request.form['name_v']
-        print(data) 
-        conn = sqlite3.connect('wallpapers.db')
-        cur = conn.cursor()
-        cur.execute('INSERT INTO photos (tag) VALUES (?)', (data,))                                                                                                                                                               
+@app.route("/") 
+def home():                                                                                               
     return render_template("layout.html")
 
 
-@app.route("/upload.html")
+@app.route("/upload", methods=['GET', 'POST'])
 def upload():
     conn = sqlite3.connect("wallpapers.db")
     cur = conn.cursor()
-    data = cur.execute("SELECT tag FROM photos WHERE tag='d'")                                                                                                                                                               
-    data = cur.fetchone()
-    var = data
-    print(data)
-    return render_template("layout.html", var = var[0])
+    tag = request.form['name_v']
+    data = cur.execute("INSERT INTO photos ('tag') VALUES (?)", (tag,))
+    cur.connection.commit()   
+    print(tag)                                                                                                                                                            
+    return render_template("layout.html")
+
+@app.route("/contact")
+def contact(): 
+    return render_template("contact.html")
+
+
+@app.route("/fileupload", methods=['GET', 'POST'])
+def fileupload(): 
+    if request.method == 'POST': 
+        conn = sqlite3.connect("wallpapers.db")
+        cur = conn.cursor()
+        tag = request.form['tag']
+        file = request.files['filename']
+        
+
+        data = cur.execute("INSERT INTO photos ('tag', 'image') VALUES (?, ?)", (tag,file))
+        cur.connection.commit()   
+    return render_template("fileupload.html")
+
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
+
+
